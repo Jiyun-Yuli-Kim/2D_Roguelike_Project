d987f61c4b232401a7a7a2c233b9ccbcb9db1b95
Jiyun-Juli-Kim <juliyooni@gmail.com>
2025-02-05 00:37:52 +0900

    [Fix] 분할 로직 수정
    일정 너비 이하일 시 더이상 분할하지 않게 하는 로직 추가

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -10,6 +10,7 @@ public class MapGenerator : MonoBehaviour
     [SerializeField] float _minRate; // 이등분할 때의 최소비율
     [SerializeField] float _maxRate; // 이등분할 때의 최대비율
     [SerializeField] float _divideRate;
+    [SerializeField] float _minArea;
     [SerializeField] Tile _roomTile; // 방 안쪽 타일
     [SerializeField] Tile _wallTile; // 경계를 나타내는 타일
     [SerializeField] Tile _outTile; // 외부를 나타내는 타일
@@ -63,7 +64,12 @@ public class MapGenerator : MonoBehaviour
         {
             return;
         }
-        
+
+        if (node.nodeRect.width * node.nodeRect.height < _minArea)
+        {
+            return;
+        }
+
         var lineRenderer = Instantiate(_line).GetComponent<LineRenderer>();
         lineRenderer.useWorldSpace = true;
 
@@ -83,40 +89,45 @@ public class MapGenerator : MonoBehaviour
         if (randomDir)
         {
             node._isHorizontalCut = true;
-            int maxLength = node.nodeRect.height;
-            int minLength = node.nodeRect.width;
-            int split = Mathf.RoundToInt(maxLength * Random.Range(_minRate, _maxRate));
-            Debug.Log(split);
+            int height = node.nodeRect.height;
+            int width = node.nodeRect.width;
+            int split = Mathf.RoundToInt(height * Random.Range(_minRate, _maxRate));
             
             lineRenderer.positionCount = 2;
             lineRenderer.SetPosition(0, new Vector3(node.nodeRect.xMin, node.nodeRect.yMin+split, 0));
-            lineRenderer.SetPosition(1, new Vector3(node.nodeRect.xMin+minLength, node.nodeRect.yMin+split, 0));
-            node.node1 = new Node(new RectInt(node.nodeRect.xMin, node.nodeRect.yMin+split, minLength, maxLength-split));
-            node.node2 = new Node(new RectInt(node.nodeRect.xMin, node.nodeRect.yMin, minLength, split));
+            lineRenderer.SetPosition(1, new Vector3(node.nodeRect.xMin+width, node.nodeRect.yMin+split, 0));
+            node.node1 = new Node(new RectInt(node.nodeRect.xMin, node.nodeRect.yMin+split, width, height-split));
+            node.node2 = new Node(new RectInt(node.nodeRect.xMin, node.nodeRect.yMin, width, split));
             node.node1.parNode = node;
             node.node2.parNode = node;
             SplitNode(node.node1, depth+1);
             SplitNode(node.node2, depth+1);
+            
+            Debug.Log($"{node.node1.nodeRect.width}, {node.node1.nodeRect.height}");
+            Debug.Log($"{node.node2.nodeRect.width}, {node.node2.nodeRect.height}");
+
         }
         
         // divide vertically
         else if (!randomDir)
         {
             node._isHorizontalCut = false;
-            int maxLength = node.nodeRect.width;
-            int minLength = node.nodeRect.height;
-            int split = Mathf.RoundToInt(maxLength * Random.Range(_minRate, _maxRate));
-            Debug.Log(split);
+            int width = node.nodeRect.width;
+            int height = node.nodeRect.height;
+            int split = Mathf.RoundToInt(width * Random.Range(_minRate, _maxRate));
             
             lineRenderer.positionCount = 2;
             lineRenderer.SetPosition(0, new Vector3(node.nodeRect.xMin+split, node.nodeRect.yMin, 0));
-            lineRenderer.SetPosition(1, new Vector3(node.nodeRect.xMin+split, node.nodeRect.yMin+minLength, 0));
-            node.node1 = new Node(new RectInt(node.nodeRect.xMin, node.nodeRect.yMin, split, minLength));
-            node.node2 = new Node(new RectInt(node.nodeRect.xMin+split, node.nodeRect.yMin, maxLength-split, minLength));
+            lineRenderer.SetPosition(1, new Vector3(node.nodeRect.xMin+split, node.nodeRect.yMin+height, 0));
+            node.node1 = new Node(new RectInt(node.nodeRect.xMin, node.nodeRect.yMin, split, height));
+            node.node2 = new Node(new RectInt(node.nodeRect.xMin+split, node.nodeRect.yMin, width-split, height));
             node.node1.parNode = node;
             node.node2.parNode = node;
             SplitNode(node.node1, depth+1);
             SplitNode(node.node2, depth+1);
+            
+            Debug.Log($"{node.node1.nodeRect.width}, {node.node1.nodeRect.height}");
+            Debug.Log($"{node.node2.nodeRect.width}, {node.node2.nodeRect.height}");
         }
     }
 
